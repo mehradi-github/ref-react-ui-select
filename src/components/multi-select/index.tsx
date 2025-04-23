17bd52821c3cc3c2255d14d1ebe58551a97f81ee
mehradi <mehradi.info@gmail.com>
2025-04-23 19:08:02 +0200

    Adding handler for toggleDropdown #13

--- src/components/multi-select/index.tsx
+++ src/components/multi-select/index.tsx
@@ -1,4 +1,4 @@
-import { FC, Fragment, useRef, useState } from "react";
+import { FC, Fragment, useCallback, useEffect, useRef, useState } from "react";
 import "./multiselect.scss";
 import { Icons } from "../icons";
 
@@ -8,39 +8,71 @@ interface MultiSelectProps {
 const MultiSelect: FC<MultiSelectProps> = ({
   options: initialOptions = [],
 }) => {
-  const [availableOptions, setAvailableOptions] =
-    useState<string[]>(initialOptions);
+  // const [availableOptions, setAvailableOptions] =
+  //   useState<string[]>(initialOptions);
+  const [isOpen, setIsOpen] = useState(true);
 
   const refDropdown = useRef<HTMLDivElement>(null);
   const refInput = useRef<HTMLInputElement>(null);
 
+  const toggleDropdown = () => {
+    setIsOpen((prev) => !prev);
+  };
+
+  const closeDropdown = useCallback(() => {
+    setIsOpen(false);
+  }, []);
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        refDropdown.current &&
+        !refDropdown.current.contains(event.target as Node)
+      )
+        closeDropdown();
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [closeDropdown]);
+
+  useEffect(() => {
+    console.log(isOpen);
+  }, [isOpen]);
+
   return (
     <div className="dp-container" ref={refDropdown}>
-      <button>
+      <button onClick={toggleDropdown}>
         Science
         <Icons.chevronDown className="chevron" />
       </button>
 
-      <div className="menu">
-        <div className="container">
-          <input
-            ref={refInput}
-            type="text"
-            value=""
-            placeholder="Add new item and press Enter..."
-          />
-          <div className="scroll-area">
-            <div className="options">
-              <Fragment key="">
-                <button>
-                  Education 🎓
-                  <Icons.check className="check" />
-                </button>
-              </Fragment>
+      {isOpen && (
+        <div className="menu">
+          <div className="container">
+            <input
+              ref={refInput}
+              type="text"
+              value=""
+              onChange={() => {}}
+              placeholder="Add new item and press Enter..."
+            />
+            <div className="scroll-area">
+              <div className="options">
+                <Fragment key="">
+                  <button>
+                    Education 🎓
+                    <Icons.check className="check" />
+                  </button>
+                </Fragment>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
